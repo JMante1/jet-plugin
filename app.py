d4d5bb1931a17cd391ad32eb81f24940a237cb1d
JMante1 <40006518+JMante1@users.noreply.github.com>
2020-04-22 09:52:20 -0600

    update sankey run

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 #FLASK_APP=app.py flask run
 from flask import Flask, request, abort
+from input_data import input_data
+from find_role_name import find_role_name
+from sankey import sankey
+from sankey_graph import sankey_graph
+from retrieve_html import retrieve_html
 from Full_v004_20190506 import *
+import os
 app = Flask(__name__)
 
 #flask run --host=0.0.0.0
@@ -23,14 +29,38 @@ def wrapper():
         #instance = "synbiohub.org"
         print(url)
         print(instance)
-        #instance = instance.replace('https://', '').replace('/','')
-        narcissus, displayid, humanname, parttype, narcissuscount = inputdata(toplevel, instance)
         
+        #instance = 'https://synbiohub.org/'
+        #url = 'https://synbiohub.org/public/igem/BBa_B0012/1'
+        #top_level = url
+
+        
+        #cwd = os.getcwd()
+        #print(cwd)
+
+        #retrieve information about the poi
+        self_df, display_id, title, role, count = input_data(top_level, instance)
+
+        #Find the role name in the ontology of the part of interest
+        role_link = find_role_name(role, plural = False)
+
+        #create data for the sankey diagram and format it correctly
+        df_sankey = sankey(url, title, instance)
+
+        sankey_title = "Parts Co-Located with "+ title + " (a "+role_link+")"
+        filename= f'{cwd}\\percentcomponentsfrom_{display_id}_.html'
+
+        #create the sankey diagram
+        sankey_graph(filename, df_sankey, display_id, 'Node, Label',
+                    'Link', 'Color', 'Source','Target', 'Value',
+                    'Link Color', sankey_title, url_not_name=False) 
+
+        #obtain the html from the sankey diagram
+        sankey = retrieve_html(filename)
+
+        #delete the copy of the sankey diagram on the server
+        os.remove(filename)
        
-        rolelink = findrolename(parttype)
-        print(rolelink)
-        sankey = sankeyalazach(toplevel, instance, narcissus, displayid, humanname, parttype, narcissuscount, "Parts Co-Located with "+ humanname + " (a "+rolelink+")")
-        #print(sankey)
         return sankey
     except Exception as e:
         print(e)
@@ -41,9 +71,9 @@ def wrapper():
 def imdoingfine2():
     return("Not dead Jet")
 
-@app.route("/bar/evaluate", methods=["POST", "GET"])
-def evalbar():
-    return("Not dead Jet")
+#@app.route("/bar/evaluate", methods=["POST", "GET"])
+#def evalbar():
+#    return("Not dead Jet")
 
 @app.route("/bar/evaluate", methods=["POST", "GET"])
 def evalbar():
