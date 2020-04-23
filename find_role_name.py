b2d65fb72c8635d5401cc7f1de3ea473c3b8e299
JMante1 <40006518+JMante1@users.noreply.github.com>
2020-04-22 19:00:51 -0600

    added comments to find_role_name

--- find_role_name.py
+++ find_role_name.py
@@ -3,6 +3,36 @@ from bs4 import BeautifulSoup
 from capitalise_each_word import capitalise_each_word
 
 def find_role_name(role_number, plural = False): 
+    """
+    Use the role_number (e.g. 0000167) to find the human readable name for the role and create an html link to the ontology
+    explaining the human readable name
+    
+    Requirements
+    -------
+    import requests
+    from bs4 import BeautifulSoup
+    from capitalise_each_word import capitalise_each_word
+    
+    Parameters
+    ----------
+    role_number : string
+        the sequence ontology number for the role (e.g. 0000167)
+    plural : boolean, default:False
+        Returns the html for a url with the visible text in plural (e.g. if plural=True returns Promoters instead of Promoter)
+    
+    Returns
+    -------
+    role_link: string
+        a piece of html with a clickable link (e.g. <a href='https://www.ebi.ac.uk/ols/ontologies/so/terms?obo_id=SO:0000167'>Role</a>)
+    
+    Example
+    --------
+    number = '0000316'
+    role_link = find_role_name(number, plural = True)
+    
+    Output: "<a href='https://www.ebi.ac.uk/ols/ontologies/so/terms?obo_id=SO:0000316'>CDSs</a>"   
+    """
+    
     #url to get the part name
     url = 'http://www.ontobee.org/ontology/SO?iri=http://purl.obolibrary.org/obo/SO_'+role_number
     
@@ -10,7 +40,6 @@ def find_role_name(role_number, plural = False):
     response = requests.get(url)
     
     # parse html
-    
     page = str(BeautifulSoup(response.content, "lxml"))
     
     #find the location of the start of the particular page
@@ -25,7 +54,7 @@ def find_role_name(role_number, plural = False):
     #if there are spaces in the name they are now put in (represented in the ontology using '_')
     role_name = role_name.replace("_"," ")
     
-    
+    #Capitalise Each Character after a space
     role_name = capitalise_each_word(role_name)
     
     #if flag plural add an s to the end of the role name
