415d72a5a5f178ac5b1fb8b1534bca62092f2316
JMante1 <40006518+JMante1@users.noreply.github.com>
2020-07-18 13:45:44 -0700

    Update app.py

--- app.py
+++ app.py
@@ -17,38 +17,61 @@ app = Flask(__name__)
 
 #flask run --host=0.0.0.0
 @app.route("/sankey/status")
-def imdoingfine():
-    return("Not dead Jet")
+def Sankey_Status():
+    return("The sankey plugin is up and running")
 
-@app.route("/sankey/evaluate", methods=["POST", "GET"])
-def evalsankey():
-    return("Not dead Jet")
+@app.route("/sankey/evaluate", methods=["POST"])
+def Sankey_Evaluate():
+    data = request.get_json(force=True)
+    rdf_type = data['type']
+    
+    ########## REPLACE THIS SECTION WITH OWN RUN CODE #################
+    #uses rdf types
+    accepted_types = {'ComponentDefinition'}
+    
+    acceptable = rdf_type in accepted_types
+    
+    # #to ensure it shows up on all pages
+    # acceptable = True
+    ################## END SECTION ####################################
+    
+    if acceptable:
+        return f'The type sent ({rdf_type}) is an accepted type', 200
+    else:
+        return f'The type sent ({rdf_type}) is NOT an accepted type', 415
 
 
 @app.route("/sankey/run", methods=["POST"])
-def wrapper():
-    data = request.json
-    url = data['complete_sbol'].replace('/sbol','')
-    instance = data['instanceUrl'].replace('/sbol','')
-    uri = data['top_level']
+def Sankey_Run():
+    data = request.get_json(force=True)
+    
+    top_level_url = data['top_level']
+    complete_sbol = data['complete_sbol']
+    instance_url = data['instanceUrl']
+    size = data['size']
+    rdf_type = data['type']
+    shallow_sbol = data['shallow_sbol']
+    
+    url = complete_sbol.replace('/sbol','')
+    
     try:
         
-        #instance = 'https://synbiohub.org/'
+        #instance_ur = 'https://synbiohub.org/'
         #url = 'https://synbiohub.org/public/igem/BBa_B0012/1'
-        #uri = 'https://dev.synbiohub.org/public/igem/BBa_B0012/1'
+        #top_level_url = 'https://dev.synbiohub.org/public/igem/BBa_B0012/1'
         
         #get current working directory
         cwd = os.getcwd()
 
         #retrieve information about the poi
-        self_df, display_id, title, role, count = input_data(uri, instance)
+        self_df, display_id, title, role, count = input_data(top_level_url, instance_url)
 
         #print("Find role name")
         #Find the role name in the ontology of the part of interest
         role_link = find_role_name(role, plural = False)
 
         #create data for the sankey diagram and format it correctly
-        df_sankey = sankey(url, uri, title, instance)
+        df_sankey = sankey(url, top_level_url, title, instance_url)
 
         sankey_title = "Parts Co-Located with "+ title + " (a "+role_link+")"
         filename= os.path.join(cwd, f'sankey_{display_id}_.html')
@@ -67,38 +90,58 @@ def wrapper():
         return result 
     except Exception as e:
         print(e)
-        abort(404)
+        abort(400)
 
 #flask run --host=0.0.0.0
 @app.route("/bar/status")
-def imdoingfine2():
-    return("Not dead Jet")
+def Bar_Status():
+    return("The bar plugin is up and running")
 
-#@app.route("/bar/evaluate", methods=["POST", "GET"])
-#def evalbar():
-#    return("Not dead Jet")
 
-@app.route("/bar/evaluate", methods=["POST", "GET"])
-def evalbar():
-    return("Not dead Jet")
+@app.route("/bar/evaluate", methods=["POST"])
+def Bar_Evaluate():
+    data = request.get_json(force=True)
+    rdf_type = data['type']
+    
+    ########## REPLACE THIS SECTION WITH OWN RUN CODE #################
+    #uses rdf types
+    accepted_types = {'Component'}
+    
+    acceptable = rdf_type in accepted_types
+    
+    # #to ensure it shows up on all pages
+    # acceptable = True
+    ################## END SECTION ####################################
+    
+    if acceptable:
+        return f'The type sent ({rdf_type}) is an accepted type', 200
+    else:
+        return f'The type sent ({rdf_type}) is NOT an accepted type', 415
 
 
 @app.route("/bar/run", methods=["POST"])
-def wrapper2():
-    data = request.json
-    url = data['complete_sbol'].replace('/sbol','')
-    instance = data['instanceUrl'].replace('/sbol','')
-    uri = data['top_level']
+def Bar_Run():
+    data = request.get_json(force=True)
+    
+    top_level_url = data['top_level']
+    complete_sbol = data['complete_sbol']
+    instance_url = data['instanceUrl']
+    size = data['size']
+    rdf_type = data['type']
+    shallow_sbol = data['shallow_sbol']
+    
+    url = complete_sbol.replace('/sbol','')
+
     try:
         
         #current working directory
         cwd = os.getcwd()
 
         #create input data
-        self_df, display_id, title, role, count = input_data(uri, instance)
+        self_df, display_id, title, role, count = input_data(top_level_url, instance_url)
         
         #create and format data for the most_used barchart
-        bar_df = most_used_bar(uri, instance, display_id, title, role, 
+        bar_df = most_used_bar(top_level_url, instance_url, display_id, title, role, 
                         count)
         
         #graph title for most used barchart
@@ -119,7 +162,7 @@ def wrapper2():
         #find poi role ontology link
         role_link = find_role_name(role, plural = False)
 
-        bar_df = most_used_by_type_bar(uri,instance, display_id, title, 
+        bar_df = most_used_by_type_bar(top_level_url,instance_url, display_id, title, 
                       role, count)
 
         #graph title for most used barchart
@@ -143,4 +186,4 @@ def wrapper2():
         return toggle_display
     except Exception as e:
         print(e)
-        abort(404)
+        abort(400)
